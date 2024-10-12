0f435d47943301d8c3aee2a8e24c15aa535d9e2a
heta-djs-arya <karaniheta@gmail.com>
2024-10-12 21:42:01 +0530

    'searchbar'

--- task1/lib/homepage.dart
+++ task1/lib/homepage.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  MyHomePage({super.key});
+
+  final TextEditingController searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -10,19 +13,49 @@ class MyHomePage extends StatelessWidget {
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: 300,
-        color: Color(0xFF3C2B2B),
+        decoration: BoxDecoration(
+          color: Color(0xFF3C2B2B),
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(50),
+              bottomRight: Radius.circular(50)), // Radius for rounded edges
+        ),
         child: Column(
           children: [
             Row(
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(top: 80, left: 30),
+                  padding: const EdgeInsets.only(top: 30, left: 30, bottom: 30),
                   child: CircleAvatar(
                     radius: 70,
                     backgroundImage: AssetImage('assets/profilepic.png'),
                   ),
                 )
               ],
+            ),
+            Padding(
+              
+              padding: const EdgeInsets.only(left:10,right: 10),
+              child: SearchBar(
+                controller: searchController,
+                leading: IconButton(
+                onPressed: () {}, 
+                icon: const Icon(Icons.search)
+                
+                ),
+                
+                trailing: [
+                IconButton( 
+                  onPressed: () {}, 
+                icon: const Icon(Icons.mic),
+               
+                
+                ),],
+                
+                  //padding: const EdgeInsets.only(right: 10,left: 10) ,
+                 //backgroundColor: Colors.white,
+                hintText: 'Search...',
+                // icon: Icon(Icons.search),
+              ),
             )
           ],
         ),
