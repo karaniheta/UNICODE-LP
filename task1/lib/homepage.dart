9bf2b092af49807c7f1b257f455df08399601fa3
heta-djs-arya <karaniheta@gmail.com>
2024-10-20 23:22:35 +0530

    'api1'

--- task1/lib/homepage.dart
+++ task1/lib/homepage.dart
@@ -24,11 +24,15 @@ class _MyHomePageState extends State<MyHomePage> {
     if (response.statusCode == 200) {
       for (var i in data) {
         postList.add(PostsModel.fromJson(i));
+        print("object");
       }
       return postList;
+      print(postList);
     } else {
+      print('ahmvhgdcfhgjgmnb');
       return postList;
     }
+    print(postList);
   }
 
   @override
@@ -37,111 +41,132 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Scaffold(
       body: Column(
         children: [
-          Stack(
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: 300,
-                decoration: BoxDecoration(
-                  color: Color(0xFF3C2B2B),
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(50),
-                      bottomRight:
-                          Radius.circular(50)), // Radius for rounded edges
-                ),
-                child: Column(
+          FutureBuilder(
+            future: getPostApi(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return Text('loading...');
+              } else {
+                return Stack(
                   children: [
-                    Row(
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 300,
+                      decoration: BoxDecoration(
+                        color: Color(0xFF3C2B2B),
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(50),
+                            bottomRight: Radius.circular(
+                                50)), // Radius for rounded edges
+                      ),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    top: 30, left: 30, bottom: 30),
+                                child: CircleAvatar(
+                                  radius: 70,
+                                  backgroundImage:
+                                      AssetImage('assets/profilepic.png'),
+                                ),
+                              ),
+                              Text('     abvn'),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10, right: 10),
+                            child: SearchBar(
+                              controller: searchController,
+                              leading: IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(Icons.search)),
+
+                              trailing: [
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(Icons.mic),
+                                ),
+                              ],
+
+                              //padding: const EdgeInsets.only(right: 10,left: 10) ,
+                              backgroundColor:
+                                  WidgetStateProperty.all(Colors.white),
+                              hintText: 'Search...',
+                              // icon: Icon(Icons.search),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                );
+                ListView.builder(
+                  itemCount: postList.length,
+                  itemBuilder: (context, index) {
+                    return Stack(
                       children: [
                         Padding(
                           padding: const EdgeInsets.only(
-                              top: 30, left: 30, bottom: 30),
-                          child: CircleAvatar(
-                            radius: 70,
-                            backgroundImage:
-                                AssetImage('assets/profilepic.png'),
+                              left: 10, right: 10, top: 30),
+                          child: Container(
+                            height: 150,
+                            width: 350,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
                           ),
                         ),
-                        Text('     abvn'),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10, right: 10),
-                      child: SearchBar(
-                        controller: searchController,
-                        leading: IconButton(
-                            onPressed: () {}, icon: const Icon(Icons.search)),
-
-                        trailing: [
-                          IconButton(
-                            onPressed: () {},
-                            icon: const Icon(Icons.mic),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 10, right: 10, top: 30),
+                          child: ClipPath(
+                            clipper: MyClipper(),
+                            child: Container(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 10, horizontal: 10),
+                              height: 150,
+                              width: 350,
+                              decoration: BoxDecoration(
+                                color: Color(0x86997E6B),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
                           ),
-                        ],
-
-                        //padding: const EdgeInsets.only(right: 10,left: 10) ,
-                        backgroundColor: WidgetStateProperty.all(Colors.white),
-                        hintText: 'Search...',
-                        // icon: Icon(Icons.search),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
-          Stack(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 10, right: 10, top: 30),
-                child: Container(
-                  height: 150,
-                  width: 350,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 10, right: 10, top: 30),
-                child: ClipPath(
-                  clipper: MyClipper(),
-                  child: Container(
-                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                    height: 150,
-                    width: 350,
-                    decoration: BoxDecoration(
-                      color: Color(0x86997E6B),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10, left: 200),
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(150),
-                      border: Border.all(
-                        color: Colors.black,
-                      )),
-                  child: CircleAvatar(
-                    radius: 100,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 40.0, left: 20),
-                child: Text(
-                  'Cappuccino',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              )
-            ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10, left: 200),
+                          child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(150),
+                                border: Border.all(
+                                  color: Colors.black,
+                                )),
+                            child: CircleAvatar(
+                              radius: 100,
+                                backgroundImage:
+                                  NetworkImage(postList[index].image.toString()),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 40.0, left: 20),
+                          child: Text(
+                            'Cappichino',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        )
+                      ],
+                    );
+                  },
+                );
+              }
+            },
           )
         ],
       ),
